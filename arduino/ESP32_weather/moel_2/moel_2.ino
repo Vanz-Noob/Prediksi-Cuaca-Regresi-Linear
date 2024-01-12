b58c55af89df8f4393e276577b656270a28f7fbb
Vanz-Noob <renaldivanza@gmail.com>
2024-01-12 14:36:02 +0700

    bugfixed

--- arduino/ESP32_weather/moel_2/moel_2.ino
+++ arduino/ESP32_weather/moel_2/moel_2.ino
@@ -1,53 +1,65 @@
-#include <EloquentTinyML.h>
-#include <eloquent_tinyml/tensorflow.h>
 #include <DHT.h>
-#include "sensor_model2.h"
 
-#define NUMBER_OF_INPUTS 2
-#define NUMBER_OF_OUTPUTS 1
-#define TENSOR_ARENA_SIZE 10000  // Sesuaikan jika perlu
-
-#define DHTPIN 2
-#define DHTTYPE DHT11
+#define DHTPIN 2     // Pin data sensor DHT11 terhubung ke pin 2
+#define DHTTYPE DHT11 // DHT 11
 
 DHT dht(DHTPIN, DHTTYPE);
 
-Eloquent::TinyML::TensorFlow::TensorFlow<NUMBER_OF_INPUTS, NUMBER_OF_OUTPUTS, TENSOR_ARENA_SIZE> tf;
+#define DRY_THRESHOLD 30
+#define HUMID_THRESHOLD 60
+
+// Faktor penyesuaian kelembapan
+float humidityAdjustment = 0.9;
 
 void setup() {
   Serial.begin(115200);
   dht.begin();
-  tf.begin(sensor_model2);
-
-  Serial.println("Prediksi Hujan ANN");
-  delay(3000);
 }
 
 void loop() {
-  float hum = dht.readHumidity();
-  float temp = dht.readTemperature();
-  delay(1000);
-  if (isnan(temp) || isnan(hum)) {
-    Serial.println("Gagal membaca data dari sensor DHT11");
-    return;
+  delay(2000);
+
+  // Baca data dari sensor DHT11
+  float humidity = dht.readHumidity();
+
+  // Kalibrasi kelembapan
+  humidity *= humidityAdjustment;
+
+  // Logika Fuzzy
+  int rainIntensity = fuzzyLogic(humidity);
+
+  Serial.print("Kelembapan: ");
+  Serial.print(humidity);
+  Serial.print(" %, Kondisi Hujan: ");
+
+  switch (rainIntensity) {
+    case 0:
+      Serial.println("Tidak Hujan");
+      break;
+    case 1:
+      Serial.println("Hujan Ringan");
+      break;
+    case 2:
+      Serial.println("Hujan Lebat");
+      break;
+    default:
+      Serial.println("Data Tidak Valid");
+      break;
+  }
+}
+
+int fuzzyLogic(float hum) {
+  // Inisialisasi variabel fuzzy
+  int rainIntensity = 0;
+
+  // Fuzzifikasi
+  if (hum <= DRY_THRESHOLD) {
+    rainIntensity = 0; // Tidak Hujan
+  } else if (hum <= HUMID_THRESHOLD) {
+    rainIntensity = 1; // Hujan Ringan
+  } else {
+    rainIntensity = 2; // Hujan Lebat
   }
 
-  Serial.print("Suhu: ");
-  Serial.print(temp);
-  Serial.print(" °C\t");
-  Serial.print("Kelembapan: ");
-  Serial.print(hum);
-  Serial.println(" %");
-  float input[2] = {temp, hum};
-  float outputs = tf.predict(input);
-//  tf.scaleInput(hum);  // Masukkan nilai kelembapan
-//  tf.scaleInput(temp); // Masukkan nilai suhu
-//
-//  float outputs;
-//  tf.predict({}, {&outputs});  // Prediksi dengan input yang sudah dimasukkan
-
-  Serial.print("Prediksi hujan: ");
-  Serial.println(outputs);
-
-  delay(1000);
+  return rainIntensity;
 }
--- arduino/test/test.ino
+++ arduino/test/test.ino
@@ -1,7 +1,6 @@
 #include <EloquentTinyML.h>
 #include <eloquent_tinyml/tensorflow.h>
 #include <DHT.h>
-#include <SPI.h>
 #include <Wire.h>
 #include <Adafruit_GFX.h>
 #include <Adafruit_SSD1306.h>
@@ -17,8 +16,8 @@
 #define DHTPIN 2
 #define DHTTYPE DHT11
 
-#define OLED_RESET     -1 // Reset pin # (or -1 if sharing Arduino reset pin)
-#define SCREEN_ADDRESS 0x3C ///< See datasheet for Address; 0x3D for 128x64, 0x3C for 128x32
+#define OLED_RESET -1       // Reset pin # (or -1 if sharing Arduino reset pin)
+#define SCREEN_ADDRESS 0x3C // See datasheet for Address; 0x3D for 128x64, 0x3C for 128x32
 
 Adafruit_SSD1306 display(SCREEN_WIDTH, SCREEN_HEIGHT, &Wire, OLED_RESET);
 
@@ -26,22 +25,26 @@ DHT dht(DHTPIN, DHTTYPE);
 
 Eloquent::TinyML::TensorFlow::TensorFlow<N_INPUTS, N_OUTPUTS, TENSOR_ARENA_SIZE> tf;
 
-void setup() {
+void setup()
+{
   Serial.begin(115200);
   dht.begin();
   delay(4000);
   tf.begin(sensor_model);
-  if (!tf.isOk()) {
+  if (!tf.isOk())
+  {
     Serial.print("ERROR: ");
     Serial.println(tf.getErrorMessage());
-
-    while (true) delay(1000);
+    while (true)
+      delay(1000);
   }
   Serial.println("Prediksi Hujan ANN");
   delay(3000);
-  if(!display.begin(SSD1306_SWITCHCAPVCC, SCREEN_ADDRESS)) {
+  if (!display.begin(SSD1306_SWITCHCAPVCC, SCREEN_ADDRESS))
+  {
     Serial.println(F("SSD1306 allocation failed"));
-    for(;;); // Don't proceed, loop forever
+    for (;;)
+      ; // Don't proceed, loop forever
   }
   display.display();
   delay(2000); // Pause for 2 seconds
@@ -50,33 +53,38 @@ void setup() {
   display.clearDisplay();
 }
 
-void loop() {
+void displayTriangleAnimation()
+{
+  for (int16_t i = max(display.width(), display.height()) / 2; i > 0; i -= 5)
+  {
+    // The INVERSE color is used so triangles alternate white/black
+    display.fillTriangle(
+        display.width() / 2, display.height() / 2 - i,
+        display.width() / 2 - i, display.height() / 2 + i,
+        display.width() / 2 + i, display.height() / 2 + i, SSD1306_INVERSE);
+    display.display();
+    delay(100);
+  }
+
+  delay(2000);
+}
+
+void loop()
+{
   float hum = dht.readHumidity();
   float temp = dht.readTemperature();
   float input[2] = {hum, temp};
 
-  if (isnan(temp) || isnan(hum)) {
+  if (isnan(temp) || isnan(hum))
+  {
     Serial.println("Gagal membaca data dari sensor DHT11");
     return;
   }
-// yang bagus
-//  float input[2] = {hum, temp};
-//  float outputs = tf.predict(input);
-//  delay(4000);
-
-// mengarah benar
-//  float scaledInput[2];
-//  scaledInput[0] = (hum - 0) / 100.0;  // assuming 0-100% humidity range
-//  scaledInput[1] = (temp - (-20)) / (50 - (-20));  // assuming -20 to 50°C temperature range
-//
-//  // Prediction output
-//  uint8_t output;
-//  float outputs = tf.predict((uint8_t *)scaledInput, &output);
 
   // Scale the input manually based on the range used during training
   float scaledInput[2];
-  scaledInput[0] = (hum - 0) / 100.0;  // assuming 0-100% humidity range
-  scaledInput[1] = (temp - (-20)) / (50 - (-20));  // assuming -20 to 50°C temperature range
+  scaledInput[0] = (hum - 0) / 100.0;                            // assuming 0-100% humidity range
+  scaledInput[1] = (temp - (-20)) / (50 - (-20)); // assuming -20 to 50°C temperature range
 
   // Prediction output
   float output;
@@ -88,7 +96,8 @@ void loop() {
   Serial.print("Kelembapan: ");
   Serial.print(hum);
   Serial.println(" %");
-    // Display on OLED
+
+  // Display on OLED
   display.clearDisplay();
   display.setTextSize(1);
   display.setTextColor(SSD1306_WHITE);
@@ -104,14 +113,16 @@ void loop() {
   display.print("Prediksi hujan: ");
   display.print(output);
   display.display();
-  delay(1000);
-  display.clearDisplay();
 
+  // Display triangle animation after 4 seconds
+  if (millis() > 4000 && millis() < 6000)
+  {
+    displayTriangleAnimation();
+  }
 
   // Tampilkan output
   Serial.print("Prediksi hujan: ");
   Serial.println(output);
-  
 
-  delay(4000);
+  delay(1000); // Delay 1 second for smoother display
 }
